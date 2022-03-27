8c18afa10dc19292d4d0191db69dc60b10af7626
Md Mamun <juborajvai22@gmail.com>
2022-03-27 12:40:03 +0600

    header complect

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
 import Button from 'react-bootstrap/Button'
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
-    <div className="App">
-      <h1 className='bg-light'>Hello</h1>
-      <h2>Hellow</h2>
+    <div className="book-shops">
+     <Header></Header>
      
     </div>
   );
---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+      return (
+            <div className='bg-light text-center py-2'>
+                  <h2>Book Shop</h2>
+                  <h3>Choose 4 Books</h3>
+            </div>
+      );
+};
+
+export default Header;
